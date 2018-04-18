86bed1c1e4f910f941ecb97279ce771c4c3af9ba
M. Walker Wells <github@mwalkerwells.com>
2018-04-17 17:35:22 -0700

    Updated implementation to prevent unnecessary function-as-child call

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react'
 
-export default (Placeholder) =>
+export default Placeholder =>
 	class OnLayout extends Component {
-		state = { dimensions: null }
+		state = {
+			initialLayoutRequired: true
+		}
 
-		onLayout = eventHandler => e => {
-			if (eventHandler) {
-				eventHandler(e)
+		onLayout = layoutHandler => e => {
+			const { initialLayoutRequired, dimensions } = this.state
+			const { width, height } = e.nativeEvent.layout
+			if (layoutHandler) {
+				layoutHandler(e)
 			}
-			this.setState({
-				dimensions: e.nativeEvent.layout,
-			})
+			if (initialLayoutRequired) {
+				// onLayout is sometimes called when width or height are still 0
+				if (width > 0 && height > 0) {
+					this.setState({ dimensions: { width, height }, initialLayoutRequired: false })
+				}
+			} else {
+				// onLayout is sometimes called after initial layout with the same dimensions
+				if (layout.width !== width || layout.height !== height) {
+					this.setState({ dimensions: { width, height } })
+				}
+			}
 		}
 
 		render() {
-			const { children, onLayout: eventHandler, ...props } = this.props
-			const { dimensions } = this.state
-			return dimensions
-				? children({ width: dimensions.width, length: dimensions.length })
-				: <Placeholder {...props} onLayout={this.onLayout(eventHandler)} />
+			const { children, onLayout: layoutHandler, ...props } = this.props
+			return (
+				this.state.dimensions
+					? children({ width: this.state.dimensions.width, height: this.state.dimensions.height })
+					: <Placeholder {...props} onLayout={this.onLayout(layoutHandler)} />
+			)
 		}
 	}
