34e687542ecf4f8d5f884a8eef2a33cc3d84a3cf
Reginald Johnson <reginald.johnson@gmail.com>
2019-04-26 22:15:44 -0400

    Works at a basic level now

--- index.js
+++ index.js
@@ -1,36 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-export default Placeholder =>
-	class OnLayout extends Component {
-		state = {
-			initialLayoutRequired: true
-		}
+export default (Placeholder) =>
+  class OnLayout extends Component {
+    state = {
+      initialLayoutRequired: true,
+      dimensions: undefined
+    };
 
-		onLayout = layoutHandler => e => {
-			const { initialLayoutRequired, dimensions } = this.state
-			const { width, height } = e.nativeEvent.layout
-			if (layoutHandler) {
-				layoutHandler(e)
-			}
-			if (initialLayoutRequired) {
-				// onLayout is sometimes called when width or height are still 0
-				if (width > 0 && height > 0) {
-					this.setState({ dimensions: { width, height }, initialLayoutRequired: false })
-				}
-			} else {
-				// onLayout is sometimes called after initial layout with the same dimensions
-				if (layout.width !== width || layout.height !== height) {
-					this.setState({ dimensions: { width, height } })
-				}
-			}
-		}
+    onLayout = ({
+      nativeEvent: { layout: { x, y, width, height } = {} } = {}
+    }) => {
+      debugger;
+      const { initialLayoutRequired, dimensions } = this.state;
 
-		render() {
-			const { children, onLayout: layoutHandler, ...props } = this.props
-			return (
-				this.state.dimensions
-					? children({ width: this.state.dimensions.width, height: this.state.dimensions.height })
-					: <Placeholder {...props} onLayout={this.onLayout(layoutHandler)} />
-			)
-		}
-	}
+      if (initialLayoutRequired) {
+        // onLayout is sometimes called when width or height are still 0
+      
+          this.setState(
+            {
+              dimensions: { width, height, x, y },
+              initialLayoutRequired: false
+            },
+            () => {
+              console.log('this.state: ', this.state);
+              debugger;
+            }
+          );
+        }
+     
+    };
+
+    render() {
+      const { children, ...props } = this.props;
+      console.log('this.state.dimensions:', this.state.dimensions);
+      return this.state.dimensions ? (
+        children({
+          width: this.state.dimensions.width,
+          height: this.state.dimensions.height,
+          x: this.state.dimensions.x,
+          y: this.state.dimensions.y
+        })
+      ) : (
+        <Placeholder {...props} onLayout={this.onLayout} />
+      );
+    }
+  };
